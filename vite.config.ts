038f288a061be77635d2dc8122dfff62e9bb7c38
George Shkurta <g.shkurta@takt.gr>
2025-01-17 11:16:08 +0200

    update indentation to 4 spaces, modify HTML doctype, and add unplugin-fonts dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+import vue from '@vitejs/plugin-vue'
+import unfonts from 'unplugin-fonts/vite'
+
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        unfonts({
+            google: {
+                families: [
+                    {
+                        name: 'Geist',
+                        styles: 'wght@100..900',
+                    },
+                ]
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
 })
