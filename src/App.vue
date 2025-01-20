98e2f3f0ab6558afe65495465fe7b54c029092d6
George Shkurta <g.shkurta@takt.gr>
2025-01-20 11:18:44 +0200

    feat: implement color conversion tool with hex to hsl functionality and add Button component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,65 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import { Button } from '@/components/ui/button'
+
+import { ArrowDownUp, Copy } from 'lucide-vue-next'
+import { hexToHsl } from './lib/utils'
+
+const input = ref('#fcfcfc')
+const output = ref('')
+
+function convertValue() {
+    output.value = hexToHsl(input.value)
+}
+</script>
 
 <template>
-    <div></div>
+    <div class="h-screen flex items-center justify-center">
+        <div class="max-w-xl w-full mx-auto">
+            <div class="bg-white border rounded-lg shadow-sm">
+                <div class="relative">
+                    <input
+                        v-model="input"
+                        type="text"
+                        name="input"
+                        id="input"
+                        class="bg-transparent text-center p-6 py-10 text-xl w-full outline-0"
+                        placeholder="Enter hex value"
+                        @input="convertValue"
+                    />
+                    <div class="absolute top-0 inset-x-0 flex items-center justify-end gap-x-1 p-1">
+                        <Button variant="ghost" size="icon">
+                            <Copy />
+                        </Button>
+                    </div>
+                </div>
+                <div class="flex items-center h-9 relative">
+                    <hr class="w-full" />
+                    <div class="absolute inset-0 flex items-center justify-center">
+                        <Button variant="outline" size="icon">
+                            <ArrowDownUp />
+                        </Button>
+                    </div>
+                </div>
+                <div class="relative">
+                    <input
+                        v-model="output"
+                        type="text"
+                        name="output"
+                        id="output"
+                        class="bg-transparent text-center p-6 py-10 text-xl w-full outline-0"
+                        placeholder="Enter hsl value"
+                    />
+                    <div
+                        class="absolute bottom-0 inset-x-0 flex items-center justify-end gap-x-1 p-1"
+                    >
+                        <Button variant="ghost" size="icon">
+                            <Copy />
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,46 @@ import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export function hexToHsl(hex: string): string {
+
+    // Remove the hash at the start if it's there
+    hex = hex.replace(/^#/, '');
+
+    // Parse r, g, b values
+    let r = parseInt(hex.substring(0, 2), 16);
+    let g = parseInt(hex.substring(2, 4), 16);
+    let b = parseInt(hex.substring(4, 6), 16);
+
+    // Convert to percentage
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    // Find the greatest and smallest channel values
+    const max = Math.max(r, g, b), min = Math.min(r, g, b);
+    let h, s, l = (max + min) / 2;
+
+    if (max === min) {
+        h = s = 0; // achromatic
+    } else {
+        const d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    s = s * 100;
+    s = Math.round(s);
+    l = l * 100;
+    l = Math.round(l);
+    h = Math.round(h * 360);
+
+    return `hsl(${h}, ${s}%, ${l}%)`;
 }
